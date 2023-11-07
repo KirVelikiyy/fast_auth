71b93a10d00e111e37b1f8257e1daef551abb1f6
KirVelikiy <lisov.kirill50@gmail.com>
2023-11-07 13:04:30 +0300

    add support develop environment

--- src/utils/env.py
+++ src/utils/env.py
@@ -2,6 +2,28 @@ import os
 from exceptions.develop import LoadEnvException
 
 
+development_variables = {
+    "POSTGRES_URI": "DEV_POSTGRES_URI"
+}
+
+
+def is_development_platform(*args, **kwargs) -> bool:
+    return os.name == 'nt'
+
+
+def development_env(wrapper):
+    """
+    function-decorator for replace env variables in development
+    """
+    def check_getenv_variable(key: str, *args, **kwargs):
+        if is_development_platform():
+            key = development_variables.get(key) or key
+        return wrapper(key, *args, **kwargs)
+
+    return check_getenv_variable
+
+
+@development_env
 def getenv_variable(key: str) -> str:
     value = os.getenv(key)
     if not value:
