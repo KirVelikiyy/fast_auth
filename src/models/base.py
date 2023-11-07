34ab4ecc5d8120af220871cb9b4d1559f983433f
KirVelikiy <lisov.kirill50@gmail.com>
2023-11-07 13:00:54 +0300

    fix models

--- src/models/base.py
+++ src/models/base.py
@@ -3,4 +3,5 @@ from database.database import Base
 
 
 class Model(Base):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
--- src/models/session.py
+++ src/models/session.py
@@ -4,6 +4,6 @@ from .base import Model
 
 class AuthSession(Model):
     __tablename__ = 'auth_sessions'
-    user_id = Column(ForeignKey("users.user_id"))
+    user_id = Column(ForeignKey("users.id"))
     access_token = Column(String, nullable=True)
     refresh_token = Column(String, nullable=True)
